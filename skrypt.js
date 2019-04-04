4cc0b202ab380ac66240966e8daa0928f3023c33
sk44207 <48093262+sk44207@users.noreply.github.com>
2019-04-04 15:47:35 +0200

    kalkulator
    caly kalkulator zrobiony w JS jeszcze nie funkcjonalny

--- skrypt.js
+++ skrypt.js
@@ -1,17 +1,47 @@
-const double = x => 2*x;
-// const mul= (x,y)=> x*y;
-const init = (ev, A)=>
+const tablica =
+ [
+    {text: 1, col:1, row:4},
+    {text: 0, col:'1/3', row:5},
+    {text: 2, col:2, row:4},
+    {text: 3, col:3, row:4},
+    {text: '+', col:4, row:4},
+    {text: 4, col:1, row:3},
+    {text: 5, col:2, row:3},
+    {text: 6, col:3, row:3},
+    {text: '-', col:4, row:3},
+    {text: 7, col:1, row:2},
+    {text: 8, col:2, row:2},
+    {text: 9, col:3, row:2},
+    {text: 'c', col:4, row:2},
+    {text: 'Display', col:'1/5', row:1},
+    {text: '=', col:4, row:5},
+    {text: ',', col:3, row:5}
+];
+
+
+const init = () => 
 {
-console.log('pojemnik:',document.getElementById('pojemnik'));
-console.log(ev);
-console.log(double(A));
-// console.log(mul(x,y));
-console.log('pojemnik:',document.getElementById('pojemnik').children);
+    const pojemnik = document.createElement('div');
+    pojemnik.id = "pojemnik";
 
+    tablica.forEach(el => 
+    {
+       const guzik = document.createElement('div');
+       guzik.textContent = el.text;
+       guzik.style.gridColumn=el.col;
+       guzik.style.gridRow=el.row;
+       pojemnik.appendChild(guzik);
+       if(el.text==='Display'){
+           guzik.id='Display';
+       }else{
+        guzik.addEventListener('click', ev=>{
+           const d = document.getElementById('Display');
+           d.textContent=ev.target.textContent;
+        });
+       }
+    });
 
-
+    document.body.appendChild(pojemnik);
 }
 
-window.addEventListener('DOMContentLoaded',x=>{
-    init(x, 5);
-});
+window.addEventListener('DOMContentLoaded', init);
